e96f3c36cec7adb6f4d62ea446ed73254e87df33
MiiKaa3 <m.kasumagic@gmail.com>
2026-08-21 20:04:06 +1000

    cli enters the fight

---
+++ src/cli.c
@@ -0,0 +1,7 @@
+#include "cli.h"
+#include <stdio.h>
+
+void sigma360_cli() {
+    printf("Sigma360 CLI mode activated.\n");
+    // Add your CLI logic here
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cli.h>
+
+void version_msg();
+void usage_msg();
 
 int main(int argc, char **argv) {
-    printf("Hello, CMake!\n");
+    if (argc > 1) {
+        if (strcmp(argv[1], "--version") == 0) {
+            version_msg();
+        } else if (strcmp(argv[1], "--help") == 0) {
+            usage_msg();
+        } else if (strcmp(argv[1], "--cli") == 0) {
+            sigma360_cli();
+        } else {
+            fprintf(stderr, "[ERROR] Unknown argument: %s\n", argv[1]);
+            usage_msg();
+            return 1;
+        }
+    } else {
+        fprintf(stderr, "[ERROR] No arguments provided.\n");
+        usage_msg();
+        return 1;
+    }
+
     return 0;
+}
+
+void version_msg(){
+    printf("Sigma360 version 0.1.0\n");
+}
+
+void usage_msg() {
+    printf("Usage: sigma360 [arguments]\n");
+    printf("Arguments:\n");
+    printf("  --version   Show version information\n");
+    printf("  --help      Show this help message\n");
+    printf("  --cli       Enter CLI mode\n");
 }
