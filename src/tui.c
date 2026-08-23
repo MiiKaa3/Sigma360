09da49c62c10dc3d1e416e114186c3340a59c40b
MiiKaa3 <m.kasumagic@gmail.com>
2026-08-23 11:48:04 +1000

    downloading dialog box

--- src/tui.c
+++ src/tui.c
@@ -400,9 +400,32 @@ pid_t pid = fork();
             if (!nav_descend(&nav)) {
                 list_t *l = nav_current(&nav);
                 if (nav.depth > 0 && l->count > 0) {
-                    char* dir = build_dir(root, nav.path[nav.depth]->url, 
-                            (int)l->sel + 1);
+                    char* dir = build_dir(root, nav.path[nav.depth]->url, (int)l->sel + 1);
+
+                    struct ncplane *std = notcurses_stdplane(nc);
+                    unsigned r, c;
+                    ncplane_dim_yx(std, &r, &c);
+                    unsigned bw = (c > 40) ? 40 : c;
+                    struct ncplane_options bo = {
+                        .y = (int)(r - 3) / 2, .x = (int)(c - bw) / 2, .rows = 3, .cols = bw,
+                    };
+                    struct ncplane *box = ncplane_create(std, &bo);
+                    if (box) {
+                        uint64_t ch = 0;
+                        ncchannels_set_fg_rgb(&ch, COL_SEL_FG);
+                        ncchannels_set_bg_rgb(&ch, COL_MODAL_BG);
+                        ncplane_set_base(box, " ", 0, ch);
+                        ncchannels_set_fg_rgb(&ch, COL_BORDER_ACTIVE);
+                        ncplane_perimeter_rounded(box, 0, ch, 0);
+                        ncplane_set_fg_rgb(box, COL_HELP_DESC);
+                        ncplane_set_bg_rgb(box, COL_MODAL_BG);
+                        ncplane_putstr_yx(box, 1, 2, "downloading...");
+                        notcurses_render(nc);
+                    }
+
                     sigma360_tui_watch(dir, false, "00:00:00");
+
+                    if (box) ncplane_destroy(box);
                     free(dir);
                 }
             }
